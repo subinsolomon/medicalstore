93c2a0b02b18cc20212dc04d88aa274cc7072b9b
subin <subinsolomon08@gmail.com>
2025-04-28 23:40:25 +0530

    Response user roles while logging in

---
+++ medicalstore-frontend/src/components/FileUpload/FileUpload.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './FileUpload.css'
+
+const FileUpload = ({ refreshInventory }) => {
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+
+    const handleFileUpload = () => {
+        const formData = new FormData();
+        formData.append('file', selectedFile);
+
+        fetch('http://localhost:8080/api/upload', {
+            method: 'POST',
+            body: formData,
+        })
+            .then(response => response.text())
+            .then(data => {
+                console.log(data);
+                alert(data);
+                refreshInventory();
+            })
+            .catch(error => console.error('Error uploading file:', error));
+    };
+
+    return (
+        <div className='fileUpload-outer'>
+            <h1>Upload Excel File</h1>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={handleFileUpload}>Upload</button>
+        </div>
+    );
+};
+
+export default FileUpload;
